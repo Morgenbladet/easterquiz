6650ab3aa326c2ac37b6771c4019c7d048bca0e6
Sigve Indregard <sigve@indregard.no>
2018-03-23 22:56:17 +0100

    Minimal viable product

--- src/App.js
+++ src/App.js
@@ -6,28 +6,28 @@ import items from './questions.json';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.moveNext = this.moveNext.bind(this);
+    this.updateScore = this.updateScore.bind(this);
 
-    this.state  = { current_question: 0, score: 0, questions: items.map((q, i) =>
-      <Question key={i} proceed={this.moveNext} q={q} />) }
+    this.state  = { score: 0, completed: 0, questions: items.map((q, i) =>
+      <Question key={i} updateScore={this.updateScore} q={q} n={i+1} />) }
   }
 
-  moveNext(score) {
+  updateScore(index, score) {
     this.setState((prevState, props) => ({
       score: prevState.score + score,
-      current_question: prevState.current_question + 1
+      completed: prevState.completed + 1
     }));
   }
 
   render() {
-    let curr = this.state.current_question;
-    let curr_q = this.state.questions[curr];
-    let num_q = this.state.questions.length;
     return (
       <div className="App">
         <h2>Påskequizen</h2>
-        <p>Spørsmål { curr + 1 } av { num_q }. { this.state.score } poeng. </p>
-        { this.state.questions[this.state.current_question] }
+        <div className="status">
+          <strong>{ this.state.score }</strong> poeng
+          ({ this.state.completed } fullført).
+        </div>
+        { this.state.questions }
       </div>
     );
   }
--- src/Question.js
+++ src/Question.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 class Question extends Component {
 
   constructor(props) {
     super(props);
 
-    this.state = { userAnswer: '', submitted: false };
+    this.state = { userAnswer: '', submitted: false, committed: false };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -23,38 +24,84 @@ class Question extends Component {
   }
 
   assignScore(score, event) {
-    this.setState({ score: score });
-    this.props.proceed(score);
+    this.setState({ score: score, committed: true });
+    this.props.updateScore(this.props.key, score);
     event.preventDefault();
   }
 
   render() {
     let q = this.props.q;
-    if (this.state.submitted === true) {
-      return(
-        <div className="Question">
-          <p>Du svarte: <code>{ this.state.userAnswer }</code></p>
-          <p>Riktig svar var: <code>{ q.answer }</code></p>
-          <p>Vil du si du svarte riktig?</p>
 
-          <button onClick={(e) => this.assignScore(1.0, e) }>Ja!</button>
-          <button onClick={(e) => this.assignScore(0.5, e) }>Nja, halvt poeng</button>
-          <button onClick={(e) => this.assignScore(0, e) }>Nei</button>
-        </div>
-      );
-    } else {
-      return(
-        <div className="Question">
-          <p className="question">{ q.question }</p>
-
-          <form onSubmit={this.handleSubmit}>
-            <input type="text" value={this.state.userAnswer} onChange={this.handleChange} />
-            <input type="submit" value="Sjekk svaret!" />
-          </form>
-        </div>
-      );
+    let color = 'Question ';
+    switch(this.state.score) {
+      case 1:
+        color += 'correct';
+        break;
+      case 0.5:
+        color += 'semicorrect';
+        break;
+      case 0:
+        color += 'fail';
+        break;
+      default:
+        color += 'nocolor';
+        break;
     }
+
+    const input = (
+      <div key={1}>
+        <p className="question">{ this.props.n }: { q.question }</p>
+
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.userAnswer}
+            onChange={this.handleChange} />
+          <input type="submit" value="Svaret avgitt!" />
+        </form>
+      </div>
+    );
+
+    const correct = (
+      <div key={2}>
+        <p className="question">{ this.props.n }: { q.question }</p>
+
+        <p>Du svarte: <code>{ this.state.userAnswer }</code></p>
+        <p>Riktig svar var: <code>{ q.answer }</code></p>
+        <p>Vil du si du svarte riktig?</p>
+
+        <button onClick={(e) => this.assignScore(1.0, e) }>Ja!</button>
+        <button onClick={(e) => this.assignScore(0.5, e) }>Nja, halvt poeng</button>
+        <button onClick={(e) => this.assignScore(0, e) }>Nei</button>
+      </div>
+    );
+
+    const result = (
+      <p key={3}>
+        { this.props.n }: <em>{ this.props.q.question }</em>
+        <span> </span>
+        Du svarte <code>{ this.state.userAnswer }</code>,
+        riktig svar er <code>{ this.props.q.answer }</code>
+      </p>
+    );
+
+    let show = null;
+    if (this.state.committed) {
+      show = result;
+    } else if (this.state.submitted) {
+      show = correct;
+    } else {
+      show = input;
+    }
+
+    return(
+      <div className={color}>
+        <CSSTransitionGroup
+          transitionName="fade"
+          transitionEnterTimeout={1000}>
+          { show }
+        </CSSTransitionGroup>
+      </div>
+    );
   }
 }
 
-export default Question;
+  export default Question;
